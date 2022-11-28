3ff004746ce750b20d28e2ec27b2cd6984873ca6
K0st3k696 <p.romanczuk@gmail.com>
2022-11-28 22:00:15 +0100

    Added some maths problem into the playground

---
+++ Playground/AccesingObjects/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    int a = 0;
+
+    public static void main(String[] args) {
+        int b = 1;
+        int c = b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
